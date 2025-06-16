692161641b6642c867c00db976beec7424a83e51
Khanhbroo <khanhmathias532211@gmail.com>
2025-06-16 17:44:05 +0700

    Adding Search to a React App

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 const SearchableList = ({ items }) => {
+  const [searchItem, setSearchItem] = useState("");
+
+  const searchResult = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchItem.toLowerCase())
+  );
+
+  function handleChange(event) {
+    setSearchItem(event.target.value);
+  }
+
   return (
     <div className="searchable-list">
-      <input type="search" placeholder="Search" />
+      <input type="search" placeholder="Search" onChange={handleChange} />
       <ul>
-        {items.map((item, index) => (
+        {searchResult.map((item, index) => (
           <li key={index}>{item.toString()}</li>
         ))}
       </ul>
